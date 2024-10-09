3c5280990e47b52b64b8df844d9b4c45e546f0e1
Manuel <az885877@gmail.com>
2024-10-08 21:49:19 -0600

    Documentacion y cometarios en el codigo y actualizacion de los test

--- parser/biesVisitor.js
+++ parser/biesVisitor.js
@@ -2,41 +2,82 @@
 // jshint ignore: start
 import antlr4 from 'antlr4';
 
-// This class defines a complete generic visitor for a parse tree produced by biesParser.
+/**
+* Esta clase define un visitante genérico completo para un árbol de análisis producido por biesParser.
+* @author Manuel Mora Sandi 
+* @author Derek Rojas Mendoza
+* @author Josué Vindas Pérez
+* @author Joseph León Cabezas
+*/
 
 export default class biesVisitor extends antlr4.tree.ParseTreeVisitor {
 
-	// Visit a parse tree produced by biesParser#start.
+	/**
+    * Visita un árbol de análisis producido por biesParser#start.
+    * 
+    * @param {StartContext} ctx - El contexto de la regla 'start'.
+    * @returns {*} - El resultado de visitar los hijos del contexto.
+    */
 	visitStart(ctx) {
 	  return this.visitChildren(ctx);
 	}
 
 
-	// Visit a parse tree produced by biesParser#funDef.
+	/**
+	* Visita la definición de una función en el contexto proporcionado.
+	*
+	* Esta función llama a `visitChildren` para recorrer todos los nodos hijos
+	* del contexto `ctx` dado.
+	*
+	* @param {Object} ctx - El contexto que representa la definición de la función.
+	* @returns {any} El resultado de visitar los nodos hijos del contexto.
+	*/
 	visitFunDef(ctx) {
 	  return this.visitChildren(ctx);
 	}
 
 
-	// Visit a parse tree produced by biesParser#argsDecl.
+	/**
+	* Visita la declaración de argumentos en el contexto proporcionado.
+	*
+	* Esta función llama a `visitChildren` para recorrer todos los nodos hijos
+	* del contexto `ctx` dado.
+	*
+	* @param {Object} ctx - El contexto que representa la declaración de argumentos.
+	* @returns {any} El resultado de visitar los nodos hijos del contexto.
+	*/
 	visitArgsDecl(ctx) {
 	  return this.visitChildren(ctx);
 	}
 
-
-	// Visit a parse tree produced by biesParser#inst.
+	/**
+    * Visita un árbol de análisis producido por biesParser#inst.
+    * 
+    * @param {InstContext} ctx - El contexto de la regla 'inst'.
+    * @returns {*} - El resultado de visitar los hijos del contexto.
+    */
 	visitInst(ctx) {
 	  return this.visitChildren(ctx);
 	}
 
 
-	// Visit a parse tree produced by biesParser#mnemonic.
+	/**
+    * Visita un árbol de análisis producido por biesParser#mnemonic.
+    * 
+    * @param {MnemonicContext} ctx - El contexto de la regla 'mnemonic'.
+    * @returns {*} - El resultado de visitar los hijos del contexto.
+    */
 	visitMnemonic(ctx) {
 	  return this.visitChildren(ctx);
 	}
 
 
-	// Visit a parse tree produced by biesParser#arg.
+	/**
+    * Visita un árbol de análisis producido por biesParser#arg.
+    * 
+    * @param {ArgContext} ctx - El contexto de la regla 'arg'.
+    * @returns {*} - El resultado de visitar los hijos del contexto.
+    */
 	visitArg(ctx) {
 	  return this.visitChildren(ctx);
 	}
